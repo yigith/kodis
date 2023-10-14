fa9069931e6c4e6fe2254a3c2806016565888eb3
KodFun <kodluyorumtv@gmail.com>
2023-10-15 01:10:24 +0300

    scrollable tabs

--- src/pages/Notebook/Notebook.jsx
+++ src/pages/Notebook/Notebook.jsx
@@ -1,14 +1,22 @@
 import { Button, Form, Nav } from "react-bootstrap";
 import "./Notebook.css";
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Notebook() {
   const [notes, setNotes] = useState([{
     title: 'Note 1',
-    content: ''
+    content: 'abc'
+  },
+  {
+    title: 'Note 2',
+    content: 'def'
   }]);
   const [activeNote, setActiveNote] = useState(notes[0]);
-  const ani = () => notes.indexOf(activeNote); // ani: active note index
+  const [activeKey, setActiveKey] = useState(0);
+  const textareaRef = useRef(null);
+  const newNoteLinkRef = useRef(null);
 
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
@@ -16,38 +24,80 @@ function Notebook() {
 
   const handleContentChange = (event) => {
     const newNotes = [...notes];
-    newNotes[ani()].content = event.target.value;
+    newNotes[activeKey].content = event.target.value;
     setNotes(newNotes);
   };
 
   const handleSave = () => {
     // Save the title and content to a database or file
-    console.log(`Title: ${title}\nContent: ${content}`);
   };
 
-  const handleTabClick = (event, index) => {
-    event.preventDefault();
+  const handleTabSelect = (index, event) => {
+    console.log("handleTabSelect", index);
+    if (index === "-1") {
+      event.preventDefault();
+      handleNewTabSelect(index, event);
+      return;
+    }
+
     setActiveNote(notes[index]);
-  }
+    setActiveKey(index);
+  };
+
+  const handleNewTabSelect = (index, event) => {
+    const newNotes = [...notes];
+    newNotes.push({ title: `Note ${newNotes.length + 1}`, content: '' });
+    setNotes(newNotes);
+    setActiveNote(newNotes[newNotes.length - 1]);
+    setActiveKey(newNotes.length - 1);
+    newNoteLinkRef.current.blur();
+    textareaRef.current.focus();
+  };
+
+  const promptForNewTitle = () => {
+    let newTitle = prompt("Enter a new title", activeNote.title) ?? "";
+    newTitle = newTitle.trim();
+
+    if (newTitle) {
+      const newNotes = [...notes];
+      newNotes[activeKey].title = newTitle;
+      setNotes(newNotes);
+    }
+  };
+
+  const handleNavLinkClick = (event, index) => {
+    if (index == activeKey) {
+      event.preventDefault();
+      promptForNewTitle();
+    }
+  };
 
   return (
     <div className="Notebook">
       <Form>
-        <Nav variant="tabs" defaultActiveKey={`#note-${ani()}`} className="mt-2">
-          {
-            notes.map((note, index) => {
-              return (
-                <Nav.Item key={index}  onClick={(e) => handleTabClick(e, index)}>
-                  <Nav.Link href={`#note-${index}`}>
-                    {note.title}
-                  </Nav.Link>
-                </Nav.Item>
-              );
-            })
-          }
-        </Nav>
+        <div className="notebook-nav-container">
+          <Nav activeKey={activeKey} variant="tabs" className="notebook-nav mt-2" onSelect={handleTabSelect}>
+            {
+              notes.map((note, index) => {
+                return (
+                  <Nav.Item className="notebook-nav-item" key={index}>
+                    <Nav.Link eventKey={index} onClick={(e) => handleNavLinkClick(e, index)}>
+                      {note.title}
+                    </Nav.Link>
+                  </Nav.Item>
+                );
+              })
+            }
+            <Nav.Item>
+              <Nav.Link className="new-note-tab-link" ref={newNoteLinkRef} eventKey={-1} active={false}>
+                <FontAwesomeIcon icon={faPlus} />
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+
         <Form.Group className="mb-2 flex-fill">
-          {activeNote && <Form.Control className="textarea-content" as="textarea" placeholder="Write your notes here..."
+          {activeNote && <Form.Control ref={textareaRef} className="textarea-content" as="textarea" placeholder="Write your notes here..."
             value={activeNote.content}
             onChange={handleContentChange} />}
         </Form.Group>
