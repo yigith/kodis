d952722c3b65df0d90963b96394da5f8a6326e25
KodFun <kodluyorumtv@gmail.com>
2023-10-22 04:32:46 +0300

    link copier sm

--- src/components/NavbarLinkCopier/NavbarLinkCopier.jsx
+++ src/components/NavbarLinkCopier/NavbarLinkCopier.jsx
@@ -19,8 +19,8 @@ export default function NavbarLinkCopier({ code }) {
 
   return (
     <>
-      <InputGroup className="d-flex justify-content-center">
-        <InputGroup.Text className="bg-dark px-2">
+      <InputGroup size="sm" className="d-flex justify-content-center">
+        <InputGroup.Text className="bg-dark">
           kod.is{code && `/${code}`}
         </InputGroup.Text>
         <OverlayTrigger onExited={handleExit} trigger={["hover", "focus"]} overlay={<Tooltip id="tt-copy-link">{tooltip}</Tooltip>} placement='bottom'>
