8d9965817873cdfeabc00e2e1428d8a2baed5c1d
KodFun <kodluyorumtv@gmail.com>
2023-11-06 23:49:28 +0300

    set your username

--- src/pages/StartScreen/StartScreen.jsx
+++ src/pages/StartScreen/StartScreen.jsx
@@ -1,22 +1,24 @@
-import { Alert, Badge, Button, Card, Col, Form, InputGroup, Row } from 'react-bootstrap';
+import { Button, Card, Col, Form, InputGroup, Row } from 'react-bootstrap';
 import './StartScreen.css';
 import StartScreenCard from './StartScreenCard';
 import { useContext, useState } from 'react';
-import { redirect, useNavigate, useNavigation } from 'react-router-dom';
+import { redirect, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import LoadingSpinner from '/src/components/LoadingSpinner/LoadingSpinner';
 import { AppContext } from '/src/AppContext';
 import withReactContent from 'sweetalert2-react-content';
 import Swal from 'sweetalert2';
-
 import GoogleSignIn from '../../components/GoogleSignIn/GoogleSignIn';
+import { AuthContext } from '../../AuthContext';
 
 function StartScreen() {
   const MySwal = withReactContent(Swal);
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
   const appContext = useContext(AppContext);
+  const { auth, setAuth } = useContext(AuthContext);
   const navigate = useNavigate();
   const [notebookCode, setNotebookCode] = useState('');
+  const [newUsername, setNewUsername] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleCodeSubmit = async () => {
@@ -50,15 +52,12 @@ function StartScreen() {
         <Col className='d-flex align-items-stretch'>
           <StartScreenCard className="w-100" headerTitle='Share Your Notes' onSubmit={handleCodeSubmit}>
             <Card.Text className='flex-fill d-flex text-center align-items-center'>
-              Type the code below to open an existing notebook or create a new one that can be used for 24 hours.
-            </Card.Text>
-            <Card.Text className=' text-center'>
-              Ex: <mark>https://kod.is/a3Bq7D</mark>
+              Open an existing notebook or create a new one that will be live for 24 hours.
             </Card.Text>
             <Row className='mt-auto'>
               <Col sm="6" md="12" lg="7">
                 <InputGroup className='mb-2 mb-sm-0 mb-md-2 mb-lg-0'>
-                  <Form.Control type="text" placeholder="Ex: a3Bq7D" value={notebookCode} onChange={e => setNotebookCode(e.target.value)} required />
+                  <Form.Control type="text" placeholder="AbcXy1" value={notebookCode} onChange={e => setNotebookCode(e.target.value)} required />
                   <Button type="submit">Open</Button>
                 </InputGroup>
               </Col>
@@ -69,17 +68,39 @@ function StartScreen() {
           </StartScreenCard>
         </Col>
         <Col className='d-flex align-items-stretch'>
-          <StartScreenCard className="w-100" headerTitle='Get Your Own Notebook' onSubmit={handleCreateClick}>
-            <div className='flex-fill d-flex flex-column justify-content-center'>
-              <Card.Text className='text-center'>
-                Create your own notebook with your username for <strong>free</strong> by signing in with Google!
+          {
+            !auth.loggedIn &&
+            <StartScreenCard className="w-100" headerTitle='Get Your Own Notebook' onSubmit={handleCreateClick}>
+              <div className='flex-fill d-flex flex-column justify-content-center'>
+                <Card.Text className='text-center'>
+                  Create your own notebook with your username for <strong>free</strong> by signing in with Google!
+                </Card.Text>
+                <Card.Text className=' text-center'>
+                  Ex: <mark>https://kod.is/@username</mark>
+                </Card.Text>
+              </div>
+              <GoogleSignIn className="mt-2" />
+            </StartScreenCard>
+          }
+          {
+            auth.loggedIn && !auth.user?.username &&
+            <StartScreenCard className="w-100" headerTitle='Set Your Username' onSubmit={handleSaveUsernameClick}>
+              <Card.Text className='flex-fill d-flex text-center align-items-center'>
+                Enter a username to create your own notebook.
               </Card.Text>
-              <Card.Text className=' text-center'>
-                Ex: <mark>https://kod.is/@username</mark>
-              </Card.Text>
-            </div>
-            <GoogleSignIn className="mt-2" />
-          </StartScreenCard>
+              <Row className='mt-auto justify-content-center'>
+                <Col sm="6" md="12" lg="7">
+                  <InputGroup className='mb-2 mb-sm-0 mb-md-2 mb-lg-0'>
+                    <InputGroup.Text>
+                      kod.is/@
+                    </InputGroup.Text>
+                    <Form.Control type="text" placeholder="username.." value={newUsername} onChange={e => setNewUsername(e.target.value)} required />
+                    <Button type="submit">Save</Button>
+                  </InputGroup>
+                </Col>
+              </Row>
+            </StartScreenCard>
+          }
         </Col>
       </Row>
     </div>
